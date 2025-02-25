d8bf9567b45011a85805412f625b52b899bd47fd
문태준 <tj.mun@okestro.com>
2025-02-25 19:03:38 +0900

    feat: Enum 활용하여 출력 관심사 이관하기
    - 입력 문자열을 UserAction Enum으로 명시적 관리
    - 셀의 상태를 Cell 개념 객체가 아닌 OutputHandler가 책임을 가지도록 CellSnapshotStatus Enum 개발

--- src/main/java/cleancode/minesweeper/tobe/GameBoard.java
+++ src/main/java/cleancode/minesweeper/tobe/GameBoard.java
@@ -77,6 +77,12 @@ public class GameBoard {
                 || cellPosition.isColIndexMoreThanOrEqual(colSize);
     }
 
+    public CellSnapshot getSnapshot(CellPosition cellPosition) {
+        Cell cell = findCell(cellPosition);
+        return cell.getSnapshot();
+    }
+
+
     public void initializeGame() {
         CellPositions cellPositions = CellPositions.from(board);
 
@@ -113,11 +119,6 @@ public class GameBoard {
 
     private void updateCellAt(CellPosition position, Cell cell) {
         board[position.getRowIndex()][position.getColIndex()] = cell;
-    }
-
-    public String getSign(CellPosition cellPosition) {
-        Cell cell = findCell(cellPosition);
-        return cell.getSign();
     }
 
     private Cell findCell(CellPosition cellPosition) {
